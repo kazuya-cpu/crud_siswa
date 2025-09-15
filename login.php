4fd9061ed27d2e3f2464a2251d54adc3745aee66
kazuya_cpu <aimmuhaimin13@gmail.com>
2025-09-15 07:52:43 +0700

    menghapus folder

--- login.php
+++ login.php
@@ -3,15 +3,15 @@ session_start();
 include 'koneksi.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $username = mysqli_real_escape_string($koneksi, $_POST['username']);
+    $password = mysqli_real_escape_string($koneksi, $_POST['password']);
 
     $query = "SELECT * FROM users WHERE username='$username' AND password= ('$password')";
-    $result = mysqli_query($conn, $query);
+    $result = mysqli_query($koneksi, $query);
 
     if (mysqli_num_rows($result) == 1) {
         $_SESSION['username'] = $username;
-        header("Location: dashboard.php");
+        header("Location: index.php");
         exit();
     } else {
         $error = "Username atau password salah!";
